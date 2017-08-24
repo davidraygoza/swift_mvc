ea25c3b3ec2a0b550d3f5e086caac05f202fa0fb
David Raygoza <david.raygoza.ramirez@gmail.com>
2017-08-24 08:52:37 -0500

    Initial Commit

---
+++ 08_example_mvc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  08_example_mvc
+//
+//  Created by David Raygoza on 24/08/17.
+//  Copyright © 2017 David Raygoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
